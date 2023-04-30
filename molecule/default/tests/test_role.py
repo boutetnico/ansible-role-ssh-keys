9738e328fc77e7de15f26281ed929b7686e87154
Nicolas Boutet <amd3002@gmail.com>
2023-04-30 11:33:01 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -6,13 +6,21 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('username,groupname,path,key', [
-  ('root', 'root', '/root/.ssh/authorized_keys',
-   'ssh-rsa PUBLIC KEY 1 dummy@molecule'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path,key",
+    [
+        (
+            "root",
+            "root",
+            "/root/.ssh/authorized_keys",
+            "ssh-rsa PUBLIC KEY 1 dummy@molecule",
+        ),
+    ],
+)
 def test_authorized_keys_file(host, username, groupname, path, key):
     f = host.file(path)
     assert f.exists
@@ -22,9 +30,12 @@ def test_authorized_keys_file(host, username, groupname, path, key):
     assert f.contains(key)
 
 
-@pytest.mark.parametrize('username,groupname,path,name', [
-  ('root', 'root', '/root/.ssh/known_hosts', 'github.com'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path,name",
+    [
+        ("root", "root", "/root/.ssh/known_hosts", "github.com"),
+    ],
+)
 def test_known_hosts_file(host, username, groupname, path, name):
     f = host.file(path)
     assert f.exists
@@ -34,10 +45,13 @@ def test_known_hosts_file(host, username, groupname, path, name):
     assert f.contains(name)
 
 
-@pytest.mark.parametrize('username,groupname,path,mode,key', [
-  ('root', 'root', '/root/.ssh/id_rsa_deploy', 0o600, 'PRIVATE KEY 2'),
-  ('root', 'root', '/root/.ssh/id_rsa_deploy.pub', 0o644, 'PUBLIC KEY 2'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path,mode,key",
+    [
+        ("root", "root", "/root/.ssh/id_rsa_deploy", 0o600, "PRIVATE KEY 2"),
+        ("root", "root", "/root/.ssh/id_rsa_deploy.pub", 0o644, "PUBLIC KEY 2"),
+    ],
+)
 def test_keys_file(host, username, groupname, path, mode, key):
     f = host.file(path)
     assert f.exists
